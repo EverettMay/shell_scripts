78e794caab420b7cff9dc6539bab73e39abf44f8
Everett <everettmay1025@gmail.com>
2025-01-30 19:39:20 -0500

    auto update

---
+++ ghupdate.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/zsh
+
+cd /home/Desktop/everything
+git add .
+git cm "auto update"
+git push
+
+cd /home/Desktop/homework
+git add .
+git cm "auto update"
+git push
+
+cd /home/Desktop/dotfiles
+git add .
+git cm "auto update"
+git push
+
+cd /home/Desktop/TA_files
+git add .
+git cm "auto update"
+git push
+
+cd /home/Desktop/cheatsheets
+git add .
+git cm "auto update"
+git push
