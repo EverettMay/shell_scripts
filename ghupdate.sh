68bdbcc13627b584ef5eef91c7fffd5c39f6154a
Everett <everettmay1025@gmail.com>
2025-02-05 21:46:09 -0500

    auto update

--- ghupdate.sh
+++ ghupdate.sh
@@ -25,3 +25,63 @@ cd /home/everett/Desktop/cheatsheets
 git add .
 git cm "auto update"
 git push
+
+cd /home/everett/Desktop/wallpaper
+git add .
+git cm "auto update"
+git push
+
+cd /home/everett/Desktop/komowataharuka
+git add .
+git cm "auto update"
+git push
+
+cd /home/everett/Desktop/CLRS_for_dummies
+git add .
+git cm "auto update"
+git push
+
+cd /home/everett/Desktop/projects/website
+git add .
+git cm "auto update"
+git push
+
+
+cd /home/everett/Desktop/projects/shell_scripts
+git add .
+git cm "auto update"
+git push
+
+
+cd /home/everett/Desktop/projects/resume
+git add .
+git cm "auto update"
+git push
+
+
+cd /home/everett/Desktop/projects/shell
+git add .
+git cm "auto update"
+git push
+
+
+cd /home/everett/Desktop/projects/blog
+git add .
+git cm "auto update"
+git push
+
+
+cd /home/everett/Desktop/projects/every_data_structures-algorithms_implementations
+git add .
+git cm "auto update"
+git push
+
+cd /home/everett/Desktop/projects/uwu
+git add .
+git cm "auto update"
+git push
+
+cd /home/everett/Desktop/projects/SMS
+git add .
+git cm "auto update"
+git push
